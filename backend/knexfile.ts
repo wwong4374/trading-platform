8b8b94f8c75390ce2268eab69d761d810ff4a1f7
Wilson Wong <wwong4374@MacBook-Pro.local>
2024-12-24 10:31:20 -0800

    created stonks DB

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -8,6 +8,7 @@ const config: { [key: string]: Knex.Config } = {
     client: 'pg',
     connection: {
       host: process.env.DB_HOST,
+      port: 5432,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
