47952badfddd4b01d5343b271701bd09abd104ea
wwong4374 <wilson.ka.wong@gmail.com>
2024-12-24 13:47:41 -0800

    add model

---
+++ backend/src/models/base/base.ts
@@ -0,0 +1,5 @@
+export interface BaseItem {
+  id: string;
+  created: Date;
+  updated: Date;
+}
---
+++ backend/src/models/securities/security.ts
@@ -0,0 +1,8 @@
+import { BaseItem } from "../base/base";
+
+enum Security {
+  STOCK = "stock",
+  BOND = "bond",
+  ETF = "etf",
+  CRYPTO = "crypto",
+}
