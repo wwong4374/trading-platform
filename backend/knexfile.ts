0b87041f315e630da13e6536f543e1e664e78fbb
Wilson Wong <wwong4374@MacBook-Pro.local>
2024-12-24 10:19:45 -0800

    db setup

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -1,13 +1,16 @@
 import type { Knex } from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
-      user: 'your_db_user',  // TODO: change to env variable
-      password: 'your_db_password',  // TODO: change to env variable
-      database: 'your_db_name',  // TODO: change to env variable
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       directory: './src/migrations',
