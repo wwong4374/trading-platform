f403269d242a1bb9848d5f2bedba683c44b42ece
wwong4374 <wilson.ka.wong@gmail.com>
2024-12-27 10:23:04 -0800

    default values

--- backend/src/migrations/20241224205247_add_securities_table.ts
+++ backend/src/migrations/20241224205247_add_securities_table.ts
@@ -5,17 +5,15 @@ const validTypes = ['stock', 'bond', 'etf', 'crypto'];
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable(tableName, (table) => {
-    table.uuid('id').primary().notNullable(),
-    table.timestamp('created').notNullable(),
-    table.timestamp('updated').notNullable(),
-    table.text('ticker').notNullable(),
-    table.text('name').notNullable(),
-    table.text('type').notNullable().checkIn(validTypes)
-  })
+    table.uuid('id').primary().notNullable().defaultTo(knex.fn.uuid());
+    table.timestamp('created').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated').notNullable().defaultTo(knex.fn.now());
+    table.text('ticker').notNullable();
+    table.text('name').notNullable();
+    table.text('type').notNullable().checkIn(validTypes);
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable(tableName);
 }
-
