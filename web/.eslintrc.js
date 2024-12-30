a1ebf0567c69f051b6589abecd26fbba52621435
wwong4374 <wilson.ka.wong@gmail.com>
2024-12-30 11:23:48 -0800

    more setup

---
+++ web/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ web/src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div>
+      <h1>Trading Platform</h1>
+    </div>
+  );
+}
